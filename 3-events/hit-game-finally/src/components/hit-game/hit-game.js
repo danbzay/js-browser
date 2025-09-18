d7c896b68750efb32130b12006ff692e5402c259
danbzay <danbzay@gmail.com>
2025-09-18 11:31:16 +0700

    rebuild 2-dom

---
+++ 3-events/hit-game-finally/src/components/hit-game/hit-game.js
@@ -0,0 +1,75 @@
+import './hit-game.css';
+
+import goblinImage from './goblin.png';
+
+export class HitGame {
+  constructor(board) {
+    if(typeof board === 'string') {
+      board = document.querySelector(board);
+    }
+
+
+    this._board = board;
+    this._renderBoard();
+    this._hits = 0;
+    this._misses = 0;
+
+    this.onBoardClick = this.onBoardClick.bind(this);
+  }
+
+  _renderBoard() {
+    this._board.classList.add('board');
+    for (let i = 0; i < 16; i++) {
+      let hole = document.createElement("div");
+      hole.classList.add("hole");
+      hole.setAttribute("data-index", i);
+      this._board.appendChild(hole);
+    }
+  }
+
+  start() {
+    this._goblinHead = new Image();
+    this._goblinHead.classList.add("goblin");
+    this._goblinHead.src = goblinImage;
+    this._board.addEventListener('click', this.onBoardClick());
+    setInterval( () => {
+      let goblinHoleIndex = Math.floor(Math.random()*15);
+      this._board.querySelector(`[data-index="${ goblinHoleIndex }"]`)
+      .appendChild(this._goblinHead);
+    }, 1000);
+  }
+
+  onBoardClick() {
+    this._board.classList.toggle('hammer-down');
+    if(this._timeout) {
+      return;
+    }
+    this._timeout = setTimeout(() => {
+      this._board.classList.toggle('hammer-down');
+      if (this._goblinHead.matches(':hover')) {
+        this.hits++;
+      } else {
+        this._miss();
+      }
+    }, 200);
+  }
+
+  _miss() {
+    if (this._misses == 4) {
+      this._gameOver();
+    } else {
+      this._misses++; 
+    }
+  }
+
+  _gameOver() {
+    this._goblinHead.remove();
+    this._board.removeEventListener('click', this.oBoardClick);
+    this._gameOver = document.createElement('div')
+      .appendTextNode(`!!!GAME OVER!!! \n You hit ${this._hits} times`);
+    this._gameOver.classList.add('game-over');
+    this._board.appendChild(this.gameOver);
+
+  }
+
+}
---
+++ 3-events/hit-game-finally/src/js/app.js
@@ -0,0 +1,7 @@
+import { HitGame } from '../components/hit-game/hit-game';
+
+const board = document.createElement('div');
+document.body.appendChild(board);
+const game = new HitGame(board);
+game.start();
+
