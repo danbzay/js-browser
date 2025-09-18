b2c337c2a4226cb8dcd2cd788c133065a8a41565
danbzay <danbzay@gmail.com>
2025-09-18 11:30:38 +0700

    rebuild 2-dom

--- 3-events/src/js/app.js
+++
@@ -1,7 +0,0 @@
-import { HitGame } from '../components/hit-game/hit-game';
-
-const board = document.createElement('div');
-document.body.appendChild(board);
-const game = new HitGame(board);
-game.start();
-
