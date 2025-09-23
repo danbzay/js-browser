cdb920550535f8b9976b6d6f4b5a4df02a6071dd
danbzay <danbzay@gmail.com>
2025-09-23 19:17:10 +0700

    3hw?

--- 3-events/tasks/src/js/app.js
+++ 3-events/tasks/src/js/app.js
@@ -2,6 +2,6 @@ import { TaskList } from "../components/task-list/task-list";
 import { FilterWidget } from "../components/filter-widget/filter-widget";
 
 const taskList = new TaskList('.app');
-const filterWidget = new FilterWidget('.filter-widget', 
-  taskList.filterHandler, taskList.submitHandler);
+new FilterWidget('.filter-widget', taskList.filterHandler, 
+  taskList.submitHandler);
 
